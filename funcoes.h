4362b713125c26a8043b4220f0012239c6c48a6e
StreeterPT <114516902+StreeterPT@users.noreply.github.com>
2023-05-31 21:26:26 +0100

    Add files via upload

--- funcoes.h
+++ funcoes.h
@@ -46,9 +46,9 @@ void check_if_there_is_reservation_and_erase(listaReservas* lista, char nome[MAX
                 printf("\tLavagem\n");
             }
             if (res->tipo == 2) {
-                printf("\tManutenção\n");
+                printf("\tManutencao\n");
             }
-            printf("Deseja cancelar esta reserva?\n\n1. Sim\n2. Não\n");
+            printf("Deseja cancelar esta reserva?\n\n1. Sim\n2. Nao\n");
             scanf("%d", &user_inp);
 
             if (user_inp == 1) {
@@ -333,12 +333,68 @@ void advance_time_with_pre_reservations(listaReservas* lista, lista_Pre_Reservas
 
 
 
+void writePreReservationsToFile(lista_Pre_Reservas* lista){
+    
+    FILE* file = fopen("Dados Pre Reservas.txt", "w");
+    if (file == NULL) {
+        printf("Failed to open the file.\n");
+        return;
+    }
+
+    fprintf(file, "Dados do Sistema de Reservas\n");
+    fprintf(file, "Pre Reservas:\n");
+    Pre_Reserva* current = lista->head;
+    while (current != NULL) {
+        const char* tipoString = (current->tipo == 1) ? "lavagem" : "manutencao";
+        fprintf(file, "%02d/%02d/%04d %02d:%02d - %s %s %s %s\n",
+                current->dia, current->mes, current->ano,
+                current->hora, current->minutos,
+                current->cliente->nome, current->cliente->apelido,current->cliente->n_telemovel,tipoString);
+        
+        current = current->next;
+    }
+fclose(file);
+
+}
+
+void readPreReservationsFromFile(lista_Pre_Reservas* lista) {
+    FILE* file = fopen("Dados Pre Reservas.txt", "r");
+    if (file == NULL) {
+        printf("Failed to open the file.\n");
+        return;
+    }
+
+    char line[100];
+    fgets(line, sizeof(line), file);  // Read the header line
+    fgets(line, sizeof(line), file);  // Read the "Pre Reservas:" line
+
+    while (fgets(line, sizeof(line), file) != NULL) {
+        if (line[0] == '\n')  // Skip empty lines
+            continue;
+        Cliente* cliente = (Cliente*) malloc(sizeof(Cliente));
+        int dia, mes, ano, hora, minutos, tipo;
+        char tipoString[15];
+
+        sscanf(line, "%2d/%2d/%4d %2d:%2d - %[^ ] %[^ ] %[^ ] %[^ ]",
+               &dia, &mes, &ano, &hora, &minutos, cliente->nome, cliente->apelido, cliente->n_telemovel, tipoString);
+
+        if (strcmp(tipoString, "manutencao") == 0) {
+            tipo = 2;
+        } else {
+            tipo = 1;
+        }
+
+        // Assuming you have a function named 'add_Pre_Reserva' to add pre-reservations
+        add_Pre_Reserva_ordem(lista,cliente,ano,mes,dia,hora,minutos,tipo);
+    }
+
+    fclose(file);
+}
 
 
 
-
-void writeReservationsToFile(listaReservas* lista,lista_Pre_Reservas* lista_) {
-    FILE* file = fopen("Dados do Sistema de Reservas.txt", "w");
+void writeReservationsToFile(listaReservas* lista) {
+    FILE* file = fopen("Dados Reservas.txt", "w");
     if (file == NULL) {
         printf("Failed to open the file.\n");
         return;
@@ -351,7 +407,7 @@ void writeReservationsToFile(listaReservas* lista,lista_Pre_Reservas* lista_) {
     // Iterate over the reservation list and write the reservations
     Reserva* current = lista->head;
     while (current != NULL) {
-        const char* tipoString = (current->tipo == 1) ? "manutencao" : "lavagem";
+        const char* tipoString = (current->tipo == 1) ? "lavagem" : "manutencao";
         fprintf(file, "%02d/%02d/%04d %02d:%02d - %s %s %s %s\n",
                 current->dia, current->mes, current->ano,
                 current->hora, current->minutos,
@@ -359,93 +415,216 @@ void writeReservationsToFile(listaReservas* lista,lista_Pre_Reservas* lista_) {
         
         current = current->next;
     }
-
-    fprintf(file,"\nPre-Reservas:\n");
-    Pre_Reserva* pre_reserva = lista_->head;
-
-while(pre_reserva != NULL){
-    const char* tipoString2 = (current->tipo == 1) ? "manutencao" : "lavagem";
-        fprintf(file, "%02d/%02d/%04d %02d:%02d - %s %s %s %s\n",
-                pre_reserva->dia, pre_reserva->mes, pre_reserva->ano,
-                pre_reserva->hora, pre_reserva->minutos,
-                pre_reserva->cliente->nome, pre_reserva->cliente->apelido,pre_reserva->cliente->n_telemovel,tipoString2);
-        
-        pre_reserva = pre_reserva->next;
-        }
-
     fclose(file);
+
 }
 
 
 
-
-
-
-
-
-
-
-
-void readReservationsFromFile(listaReservas* lista, lista_Pre_Reservas* lista_) {
-    FILE* file = fopen("Dados do Sistema de Reservas.txt", "r");
+void readReservationsFromFile(listaReservas* lista) {
+    FILE* file = fopen("Dados Reservas.txt", "r");
     if (file == NULL) {
         printf("Failed to open the file.\n");
         return;
     }
 
-
-    // Read the reservations section
     char line[100];
-    fgets(line, sizeof(line), file);  // Read the title line
+    fgets(line, sizeof(line), file);  // Read the header line
     fgets(line, sizeof(line), file);  // Read the "Reservas:" line
 
     while (fgets(line, sizeof(line), file) != NULL) {
-    if (line[0] == '\n')  // Skip empty lines
-        continue;
-
-    // Check if the line contains "Pre-Reservas"
-    if (strcmp(line, "Pre-Reservas:\n") == 0)
-        break;
-
-    Cliente* cliente = (Cliente*) malloc(sizeof(Cliente));
-    int dia, mes, ano, hora, minutos, tipo;
-    char tipoString[MAX_NOME];
-
-    sscanf(line, "%2d/%2d/%4d %2d:%2d - %[^ ] %[^ ] %[^ ] %[^ ]",
-           &dia, &mes, &ano, &hora, &minutos, cliente->nome, cliente->apelido, cliente->n_telemovel, tipoString);
-
-    if (strcmp(tipoString, "manutencao") == 0) {
-        tipo = 2;
-    } else {
-        tipo = 1;
-    }
-
-    add_Reserva_ordem_oposta(lista, cliente, ano, mes, dia, hora, minutos, tipo);
-}
-
-    // Read the pre-reservations section
-    fgets(line, sizeof(line), file);  // Read the "Pre Reservas:" line
-
-     while (fgets(line, sizeof(line), file) != NULL) {
-        if (line[0] == '\n')  continue;  // Skip empty lines
-        Cliente* cliente;
-        cliente = (Cliente*) malloc(sizeof(Cliente));
+        if (line[0] == '\n')  // Skip empty lines
+            continue;
+        Cliente* cliente = (Cliente*) malloc(sizeof(Cliente));
         int dia, mes, ano, hora, minutos, tipo;
-        char tipoString2[MAX_NOME];
-
-    
+        char tipoString[15];
 
         sscanf(line, "%2d/%2d/%4d %2d:%2d - %[^ ] %[^ ] %[^ ] %[^ ]",
-               &dia, &mes, &ano, &hora, &minutos,cliente->nome, cliente->apelido, cliente->n_telemovel, tipoString2);
+               &dia, &mes, &ano, &hora, &minutos, cliente->nome, cliente->apelido, cliente->n_telemovel, tipoString);
 
-        if (strcmp(tipoString2, "manutencao") == 0) {
+        if (strcmp(tipoString, "manutencao") == 0) {
             tipo = 2;
         } else {
             tipo = 1;
         }
-       
-        add_Pre_Reserva_ordem(lista_, cliente, ano, mes, dia, hora, minutos, tipo);
+
+        // Assuming you have a function named 'add_Pre_Reserva' to add pre-reservations
+       add_Reserva_ordem_oposta(lista,cliente,ano,mes,dia,hora,minutos,tipo);
     }
 
     fclose(file);
 }
+
+
+
+
+
+
+
+
+
+
+Pre_Reserva* find_available_pre_reserva(lista_Pre_Reservas* lista_pre_reservas, int ano, int mes, int dia, int hora, int minutos, int tipo) {
+    Pre_Reserva* pre_reserva = lista_pre_reservas->head;
+    Pre_Reserva* available_pre_reserva = NULL;
+    int lowest_flag = INT_MAX;
+
+    while (pre_reserva != NULL) {
+        if (pre_reserva->ano == ano && pre_reserva->mes == mes && pre_reserva->dia == dia) {
+            int pre_reserva_minutos_inicial = (pre_reserva->hora * 60) + pre_reserva->minutos;
+            int pre_reserva_minutos_final = pre_reserva_minutos_inicial + (pre_reserva->tipo * 30);
+
+            int reserva_minutos_inicial = (hora * 60) + minutos;
+            int reserva_minutos_final = reserva_minutos_inicial + (tipo * 30);
+
+            if (pre_reserva_minutos_inicial >= reserva_minutos_inicial && pre_reserva_minutos_final <= reserva_minutos_final) {
+                if (pre_reserva->flag < lowest_flag) {
+                    lowest_flag = pre_reserva->flag;
+                    available_pre_reserva = pre_reserva;
+                }
+            }
+        }
+
+        pre_reserva = pre_reserva->next;
+    }
+
+    return available_pre_reserva;
+}
+
+
+
+void free_pre_reserva(lista_Pre_Reservas* lista_pre_reservas, Pre_Reserva* pre_reserva) {
+    if (pre_reserva == NULL) {
+        return;  // Invalid pre-reservation pointer
+    }
+
+    Pre_Reserva* current = lista_pre_reservas->head;
+    Pre_Reserva* previous = NULL;
+
+    // Find the pre-reservation in the list
+    while (current != NULL && current != pre_reserva) {
+        previous = current;
+        current = current->next;
+    }
+
+    // If pre-reservation not found
+    if (current == NULL) {
+        return;  // Pre-reservation not found in the list
+    }
+
+    // Update pointers and deallocate memory
+    if (previous != NULL) {
+        previous->next = current->next;
+    } else {
+        lista_pre_reservas->head = current->next;
+    }
+
+    free(pre_reserva);
+}
+
+Pre_Reserva* find_conflicting_pre_reserva(lista_Pre_Reservas* lista_, int ano, int mes, int dia, int hora, int minutos, int tipo) {
+    Pre_Reserva* pre_reserva = lista_->head;
+    Pre_Reserva* conflicting_pre_reserva = NULL;
+    int lowest_flag = INT_MAX;
+
+    int minutos_inicial = (hora * 60) + minutos;
+    int minutos_final = minutos_inicial + (tipo * 30);
+
+    while (pre_reserva != NULL) {
+        if (pre_reserva->ano == ano && pre_reserva->mes == mes && pre_reserva->dia == dia) {
+            int pre_reserva_minutos_inicial = (pre_reserva->hora * 60) + pre_reserva->minutos;
+            int pre_reserva_minutos_final = pre_reserva_minutos_inicial + (pre_reserva->tipo * 30);
+
+            if (!(minutos_inicial >= pre_reserva_minutos_final || minutos_final <= pre_reserva_minutos_inicial)) {
+                if (pre_reserva->flag < lowest_flag) {
+                    lowest_flag = pre_reserva->flag;
+                    conflicting_pre_reserva = pre_reserva;
+                }
+            }
+        }
+        pre_reserva = pre_reserva->next;
+    }
+
+    return conflicting_pre_reserva;  // Return the conflicting pre_reserva with the lowest flag value or NULL if no conflict is found
+}
+
+void check_if_there_is_reservation_and_erase_2(listaReservas* lista, lista_Pre_Reservas* lista_pre_reservas, char nome[MAX_NOME], char apelido[MAX_NOME]) {
+    Reserva* res = lista->head;
+    Reserva* reserva_anterior = NULL;
+    int user_inp;
+
+    while (res != NULL) {
+        if (strcmp(res->cliente->nome, nome) == 0 && strcmp(res->cliente->apelido, apelido) == 0) {
+            printf("%d/%d/%d %02d:%02d - %s %s", res->dia, res->mes, res->ano, res->hora, res->minutos, res->cliente->nome, res->cliente->apelido);
+            if (res->tipo == 1) {
+                printf("\tLavagem\n");
+            }
+            if (res->tipo == 2) {
+                printf("\tManutenção\n");
+            }
+            printf("Deseja cancelar esta reserva?\n\n1. Sim\n2. Não\n");
+            scanf("%d", &user_inp);
+
+            if (user_inp == 1) {
+                int ano = res->ano;
+                int mes = res->mes;
+                int dia = res->dia;
+                int hora = res->hora;
+                int minutos = res->minutos;
+                int tipo = res->tipo;
+                
+
+                if (reserva_anterior != NULL) {
+                    reserva_anterior->next = res->next;
+                } else {
+                    lista->head = res->next;
+                }
+
+                printf("A reserva foi cancelada.\n");
+                free(res);
+                //
+                
+                
+
+
+                Pre_Reserva* current_pre_reserva = lista_pre_reservas->head;
+                Pre_Reserva* previous_pre_reserva = NULL;
+                
+                while (current_pre_reserva != NULL) {
+                if (is_available(lista,current_pre_reserva->ano, current_pre_reserva->mes,  current_pre_reserva->dia,current_pre_reserva->hora,  current_pre_reserva->minutos, current_pre_reserva->tipo)){
+                    Cliente* cliente_ = (Cliente*) malloc(sizeof(Cliente));
+                    strcpy(cliente_->nome,current_pre_reserva->cliente->nome);
+                    strcpy(cliente_->apelido,current_pre_reserva->cliente->apelido);
+                    strcpy(cliente_->n_telemovel,current_pre_reserva->cliente->n_telemovel);
+                    add_Reserva_ordem_oposta(lista,cliente_,current_pre_reserva->ano,current_pre_reserva->mes,current_pre_reserva->dia,current_pre_reserva->hora,current_pre_reserva->minutos,current_pre_reserva->tipo);
+                    if (previous_pre_reserva == NULL) {
+                    // The current pre_reserva is the head of the list
+                    lista_pre_reservas->head = current_pre_reserva->next;
+                    } else {
+                    // The current pre_reserva is not the head of the list
+                    previous_pre_reserva->next = current_pre_reserva->next;
+                    }
+                    
+                    Pre_Reserva* next_pre_reserva = current_pre_reserva->next;
+                    free(current_pre_reserva);
+                    current_pre_reserva= next_pre_reserva;
+                     // Move to the next pre_reserva in the list
+                    printf("A reserva foi substituida por uma pre-reserva.\n");}
+                else {
+                    previous_pre_reserva = current_pre_reserva;
+                    current_pre_reserva = current_pre_reserva->next;
+                    }}
+                    break;
+                
+                
+            
+                }
+                }
+
+            
+        reserva_anterior = res;
+        res = res->next;
+        }
+
+    }
+
